a01c032ef12c9988e45a92787c709fcaf6771995
Ryan Joy <atxryan@gmail.com>
2013-11-04 12:56:16 -0600

    Limit the query parameters that are accepted

--- api/ads.js
+++ api/ads.js
@@ -5,7 +5,20 @@ exports.post = function(request, response) {
 exports.get = function(request, response) {
     var appsTable = request.service.tables.getTable('Apps');
     
-    appsTable.where(request.query).read({success: function (results) {
+    // We only want to allow the following query parameters on the data table
+    var query = {};
+    if (request.query.city) 
+    	query.city = request.query.city;
+    if (request.query.state) 
+    	query.state = request.query.state;
+    if (request.query.countryCode) 
+    	query.countryCode = request.query.countryCode;
+    if (request.query.creditRegion) 
+    	query.creditRegion = request.query.creditRegion;
+    if (request.query.platform) 
+    	query.platform = request.query.platform;
+    
+    appsTable.where(query).read({success: function (results) {
         console.log(results);
         response.send(statusCodes.OK, results);
     }})
