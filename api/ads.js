b2ccc9ed47958d428b3e61b92a683861f914c1f6
Ryan Joy <atxryan@gmail.com>
2013-11-04 13:00:08 -0600

    Tabs to spaces

--- api/ads.js
+++ api/ads.js
@@ -8,16 +8,16 @@ exports.get = function(request, response) {
     // We only want to allow the following query parameters on the data table
     var query = {};
     if (request.query.city) 
-    	query.city = request.query.city;
+        query.city = request.query.city;
     if (request.query.state) 
-    	query.state = request.query.state;
+        query.state = request.query.state;
     if (request.query.countryCode) 
-    	query.countryCode = request.query.countryCode;
+        query.countryCode = request.query.countryCode;
     if (request.query.creditRegion) 
-    	query.creditRegion = request.query.creditRegion;
+        query.creditRegion = request.query.creditRegion;
     if (request.query.platform) 
-    	query.platform = request.query.platform;
-    
+        query.platform = request.query.platform;
+        
     appsTable.where(query).read({success: function (results) {
         console.log(results);
         response.send(statusCodes.OK, results);
