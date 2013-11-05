a0107461b932aa82d462eddd891d44b46637b43c
Ryan Joy <atxryan@gmail.com>
2013-11-05 10:08:12 -0600

    Cleaning up getads.js file and adding comments.

--- public/scripts/getads.js
+++ public/scripts/getads.js
@@ -1,18 +1,20 @@
 var AE = AE || {};
 (function (global, AE, undefined) {
 
-    AE.query = AE.query || "";
-    
+    var httpRequest, ads,
+        url = "https://appenthusiasts.azure-mobile.net/api/ads/" + (AE.query || ""),
+        adWidth = AE.width || 200;
+
+    /* Go ahead and inject stylesheet into <head> unless
+       AE.injectStyles is set to false. */
     if (AE.injectStyles !== false) {
         injectStyles();
     }
-    
-    var httpRequest, ads, 
-        url =  "https://appenthusiasts.azure-mobile.net/api/ads/" + AE.query,
-        adWidth = AE.width || 200;
-        
+
+    // Fisher-Yates shuffle
     function shuffle(array) {
-        var counter = array.length, temp, index;
+        var counter = array.length,
+            temp, index;
 
         // While there are elements in the array
         while (counter--) {
@@ -24,40 +26,37 @@ var AE = AE || {};
             array[counter] = array[index];
             array[index] = temp;
         }
-
         return array;
     }
 
     function getAds() {
-        
-        if (window.XMLHttpRequest) { // Mozilla, Safari, ...
-          httpRequest = new XMLHttpRequest();
-        } else if (window.ActiveXObject) { // IE
-          try {
-            httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
-          } 
-          catch (e) {
+        if (window.XMLHttpRequest) { // Modern browsers
+            httpRequest = new XMLHttpRequest();
+        } else if (window.ActiveXObject) { // Older versions of IE
             try {
-              httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
-            } 
-            catch (e) {}
-          }
+                httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+            } catch (e) {
+                try {
+                    httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+                } catch (e) {}
+            }
         }
 
         if (!httpRequest) {
-          console.log('[ads] Cannot create an XMLHTTP instance');
-          return false;
+            console.log('[ads] Cannot create an XMLHTTP instance');
+            return false;
         }
-        
+
         httpRequest.onreadystatechange = displayAd;
         httpRequest.open('GET', url);
         httpRequest.send();
     }
 
     function displayAd() {
-        var adJSON, ad, adString;
-        var elem = document.getElementById("AEadControl");
-        var div;
+        var adJSON, ad, adString, div,
+            elem = document.getElementById("AEadControl");
+
+
         if (document.getElementById("AppEnthusiastAd") != null) {
             div = document.getElementById("AppEnthusiastAd");
         } else {
@@ -66,43 +65,46 @@ var AE = AE || {};
         }
 
         if (httpRequest.readyState === 4) {
-          if (httpRequest.status === 200) {
-            
-            adJSON = JSON.parse(httpRequest.responseText);
-            ad = shuffle(adJSON)[0];
+            if (httpRequest.status === 200) {
 
-            adString = "<a href='http://apps.microsoft.com/windows/en-us/app/" + ad.storeId + "'>" +
-                           "<img src='" + ad.tileImageUrl + "' width='" + adWidth + "' />" +
-                           "<span>Download " + ad.title + " for Windows 8 today!</span></a>"
+                adJSON = JSON.parse(httpRequest.responseText);
+                ad = shuffle(adJSON)[0];
 
-            div.innerHTML = adString;
-            elem.parentNode.insertBefore(div, elem)
+                adString = "<a href='http://apps.microsoft.com/windows/en-us/app/" + ad.storeId + "'>" +
+                    "<img src='" + ad.tileImageUrl + "' width='" + adWidth + "' />" +
+                    "<span>Download " + ad.title + " for Windows 8 today!</span></a>"
 
-          } else {
-            console.log('There was a problem with the request.');
-          }
+                div.innerHTML = adString;
+                elem.parentNode.insertBefore(div, elem)
+
+            } else {
+                console.log('There was a problem with the request.');
+            }
         }
-      }
-      
-      function injectStyles() {
+    }
+
+    function injectStyles() {
         var css = '#AppEnthusiastAd {text-align: center; }',
             head = document.getElementsByTagName('head')[0],
             style = document.createElement('style');
+            style.type = 'text/css';
         
-        style.type = 'text/css';
-        if (style.styleSheet){
-          style.styleSheet.cssText = css;
+        if (style.styleSheet) {
+            style.styleSheet.cssText = css;
         } else {
-          style.appendChild(document.createTextNode(css));
+            style.appendChild(document.createTextNode(css));
         }
-        
-        head.appendChild(style);
-      }
 
-      getAds();
-      
-      if (AE.rotate !== false) {
+        head.appendChild(style);
+    }
+
+    // Kick off the first call to the App Enthusiasts
+    getAds();
+
+    // Rotate the ads by calling getAds every 10 seconds
+    // TODO: If the query string has not changed, this should be cached.
+    if (AE.rotate !== false) {
         setInterval(getAds, AE.rotateTimer || 10000);
-      }
+    }
 
 })(window, AE);
